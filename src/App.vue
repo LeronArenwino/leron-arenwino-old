a71a56785371322db8c7998db612cf790f401fed
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-06-15 12:06:57 -0500

    :zap: LeKnowledge component was implemented

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <le-header></le-header>
     <le-about-me></le-about-me>
     <le-portfolio></le-portfolio>
+    <le-knowledge></le-knowledge>
   </div>
 </template>
 
@@ -10,10 +11,11 @@
 import LeHeader from "@/components/LeHeader";
 import LeAboutMe from "@/components/LeAboutMe";
 import LePortfolio from "@/components/LePortfolio";
+import LeKnowledge from "@/components/LeKnowledge";
 
 export default {
   name: "App",
-  components: { LeHeader, LeAboutMe, LePortfolio },
+  components: { LeHeader, LeAboutMe, LePortfolio, LeKnowledge },
 };
 </script>
 
